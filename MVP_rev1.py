21a27de1efe1f4536cb8572e8f6f502a008fdec6
MargaretRosner <rosner.margaret@gmail.com>
2017-03-03 00:33:27 -0500

    reshaped the code and made our MVP only have notes a and b

--- MVP_rev1.py
+++ MVP_rev1.py
@@ -11,191 +11,63 @@ import pyaudio
 import wave
 import sys
 
+class Shape(object):
+    def __init__(self, note, disp)
+    self.note = note
+    self.disp = False
+    pass
+
 class Controller(object):
-    def __init__(self,model):
+    def __init__(self, mode):
         self.model = model
+
     def input_event(self,event):
         if event.type == #keyboard input:
-            for model in self.models:
-                if model.contains_pt(     ):#whatever the keyboard input above was
-                    model.reset()
-                    break
-        elif event.type == #another Keyboard input
-        #same thing
+            for shape in model:
+                if shape.note == "a4"
 
+                    CHUNK = 1024
+                    if len('a4.wav') < 2:
+                        sys.exit(-1)
+                    wf = wave.open(a4.wav[1], 'rb')
+
+        elif event.type == #keyboard input:
+            for shape in model:
+                if shape.note == "b4":
+                    CHUNK = 1024
+                    if len('b4.wav') < 2:
+                        sys.exit(-1)
+                    wf = wave.open(b4.wav[1], 'rb')
+
+        #this will play the note selected by the if statement
+        # instantiate PyAudio
+        p = pyaudio.PyAudio()
+        # open stream
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        output=True)
+        # read data
+        data = wf.readframes(CHUNK)
+        # play stream (3)
+        while len(data) > 0:
+            stream.write(data)
+            data = wf.readframes(CHUNK)
+        # stop stream (4)
+        stream.stop_stream()
+        stream.close()
+        # close PyAudio (5)
+        p.terminate()
+
+        shape.disp = True
 
 class View(object):
-    def __init__(self,model):
+    def __init__(self, model):
         self.model = model
 
-    def notes():
-
-        if key == a:
-
-            CHUNK = 1024
-            if len('a4.wav') < 2:
-                sys.exit(-1)
-            wf = wave.open(a4.wav[1], 'rb')
-            # instantiate PyAudio
-            p = pyaudio.PyAudio()
-            # open stream
-            stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                            channels=wf.getnchannels(),
-                            rate=wf.getframerate(),
-                            output=True)
-            # read data
-            data = wf.readframes(CHUNK)
-            # play stream (3)
-            while len(data) > 0:
-                stream.write(data)
-                data = wf.readframes(CHUNK)
-            # stop stream (4)
-            stream.stop_stream()
-            stream.close()
-            # close PyAudio (5)
-            p.terminate()
-
-        elif key == b:
-
-                CHUNK = 1024
-                if len('b4.wav') < 2:
-                    sys.exit(-1)
-                wf = wave.open(b4.wav[1], 'rb')
-                # instantiate PyAudio
-                p = pyaudio.PyAudio()
-                # open stream
-                stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                                channels=wf.getnchannels(),
-                                rate=wf.getframerate(),
-                                output=True)
-                # read data
-                data = wf.readframes(CHUNK)
-                # play stream (3)
-                while len(data) > 0:
-                    stream.write(data)
-                    data = wf.readframes(CHUNK)
-                # stop stream (4)
-                stream.stop_stream()
-                stream.close()
-                # close PyAudio (5)
-                p.terminate
-        elif key == c:
-                CHUNK = 1024
-                if len('c4.wav') < 2:
-                    sys.exit(-1)
-                wf = wave.open(c4.wav[1], 'rb')
-                # instantiate PyAudio
-                p = pyaudio.PyAudio()
-                # open stream
-                stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                                channels=wf.getnchannels(),
-                                rate=wf.getframerate(),
-                                output=True)
-                # read data
-                data = wf.readframes(CHUNK)
-                # play stream (3)
-                while len(data) > 0:
-                    stream.write(data)
-                    data = wf.readframes(CHUNK)
-                # stop stream (4)
-                stream.stop_stream()
-                stream.close()
-                # close PyAudio (5)
-                p.terminate
-        elif key == d:
-                CHUNK = 1024
-                if len('d4.wav') < 2:
-                    sys.exit(-1)
-                wf = wave.open(d4.wav[1], 'rb')
-                # instantiate PyAudio
-                p = pyaudio.PyAudio()
-                # open stream
-                stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                                channels=wf.getnchannels(),
-                                rate=wf.getframerate(),
-                                output=True)
-                # read data
-                data = wf.readframes(CHUNK)
-                # play stream (3)
-                while len(data) > 0:
-                    stream.write(data)
-                    data = wf.readframes(CHUNK)
-                # stop stream (4)
-                stream.stop_stream()
-                stream.close()
-                # close PyAudio (5)
-                p.terminate
-        elif key == e:
-                CHUNK = 1024
-                if len('e4.wav') < 2:
-                    sys.exit(-1)
-                wf = wave.open(e4.wav[1], 'rb')
-                # instantiate PyAudio
-                p = pyaudio.PyAudio()
-                # open stream
-                stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                                channels=wf.getnchannels(),
-                                rate=wf.getframerate(),
-                                output=True)
-                # read data
-                data = wf.readframes(CHUNK)
-                # play stream (3)
-                while len(data) > 0:
-                    stream.write(data)
-                    data = wf.readframes(CHUNK)
-                # stop stream (4)
-                stream.stop_stream()
-                stream.close()
-                # close PyAudio (5)
-                p.terminate
-        elif key == f:
-                CHUNK = 1024
-                if len('f4.wav') < 2:
-                    sys.exit(-1)
-                wf = wave.open(f4.wav[1], 'rb')
-                # instantiate PyAudio
-                p = pyaudio.PyAudio()
-                # open stream
-                stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                                channels=wf.getnchannels(),
-                                rate=wf.getframerate(),
-                                output=True)
-                # read data
-                data = wf.readframes(CHUNK)
-                # play stream (3)
-                while len(data) > 0:
-                    stream.write(data)
-                    data = wf.readframes(CHUNK)
-                # stop stream (4)
-                stream.stop_stream()
-                stream.close()
-                # close PyAudio (5)
-                p.terminate
-        elif key == g:
-                CHUNK = 1024
-                if len('g4.wav') < 2:
-                    sys.exit(-1)
-                wf = wave.open(g4.wav[1], 'rb')
-                # instantiate PyAudio
-                p = pyaudio.PyAudio()
-                # open stream
-                stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                                channels=wf.getnchannels(),
-                                rate=wf.getframerate(),
-                                output=True)
-                # read data
-                data = wf.readframes(CHUNK)
-                # play stream (3)
-                while len(data) > 0:
-                    stream.write(data)
-                    data = wf.readframes(CHUNK)
-                # stop stream (4)
-                stream.stop_stream()
-                stream.close()
-                # close PyAudio (5)
-                p.terminate
-        else:
-            return False
+    def shape():
+        if shape.disp = True:
+        #draw that specific shape
 
 
 
@@ -203,6 +75,12 @@ if __name__ ==  "__main__":
     pygame.init()
     screen = pygame.display.set_mode((800, 600)) # window size, as a tuple
     pygame.display.set_caption('Maggie and Aurora')
+
+    shape1 = Shape("a4")
+    shape2 = Shape("b4")
+
+    model = [shape1,shape2]
+
 
     running = True
     while running:
